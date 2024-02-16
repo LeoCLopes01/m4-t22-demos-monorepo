2f651026be2f10f363e070c87f07c7942d0902e6
Chrystian Rodolfo <chrystian@kenzie.com.br>
2024-02-16 10:12:23 -0300

    added m4-sp2-demo1-manha initial template

---
+++ 05 - Projeto PoKenzie - Organização - manhã/src/scripts/pokeDatabase.js
@@ -0,0 +1,32 @@
+const pokemonList = [
+  {
+    id: 1,
+    name: "bulbassaur",
+    img: "../assets/bulbassaur.svg",
+    types: ["grass", "poison"],
+    hp: 45,
+    attack: 25,
+    defense: 25,
+    speed: 25,
+  },
+  {
+    id: 2,
+    name: "raichu",
+    img: "../assets/raichu.svg",
+    types: ["eletric"],
+    hp: 60,
+    attack: 90,
+    defense: 55,
+    speed: 110,
+  },
+  {
+    id: 3,
+    name: "squirtle",
+    img: "../assets/squirtle.svg",
+    types: ["water"],
+    hp: 60,
+    attack: 48,
+    defense: 65,
+    speed: 43,
+  },
+];
